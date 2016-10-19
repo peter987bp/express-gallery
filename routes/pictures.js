35cb232a7e17778bcfe98d471a41485bcc63631c
peter987bp <peter987bp@gmail.com>
2016-10-18 19:29:31 -1000

    routes working

--- routes/pictures.js
+++ routes/pictures.js
@@ -8,12 +8,11 @@ var Picture = db.Picture;
 
 const routeOf = 'pictures';
 const userName = 'userName';
-//works
+//---HOMEPAGE
 app.get('/', function(req, res) {
   Picture.findAll({
     limit: 4
   })
-
     .then(function (pictures) {
       console.log('pictures.title: ', pictures[0].url);
       // console.log('pictures: ', pictures);
@@ -33,18 +32,50 @@ app.post('/', function (req, res) {
       res.json(user);
     });
 });
+//-------BY ID
+app.get('/:id', function(req,res){
+  Picture.findById(req.params.id)
+    .then(function (pictures) {
+    console.log('pictures: ', pictures);
+    res.render('picture_id', {
+      pictures: pictures,
+      routeOf,
+      headline: 'headline',
+      listType: 'listType',
+      userName
+    });
+  });
 
+});
 
-app.put('/:id', function(req,res) {
-      Picture.update({title: req.body.title, url:req.body.url, rating: req.body.rating},{where: { id: req.params.id}});
+//-------BY ID/edit
+app.get('/:id/edit',(req,res) =>{
+  Picture.findById(req.params.id)
+    .then(function (pictures) {
+    res.render('picture_id_edit', {
+      pictures: pictures,
+      routeOf,
+      headline: 'headline',
+      listType: 'listType',
+      userName
+    });
+  });
+});
+app.put('/:id/edit',(req,res)=> {
+  console.log('req.body: ', req.body);
+  console.log('req.body.id: ', req.body.id);
+  Picture.update({title: req.body.title, url:req.body.url, rating: req.body.rating},{where: { id: req.params.id}})
+  .then((pictures)=>{
+    res.redirect(`/gallery/${req.params.id}/edit`);
+  });
 });
 
 
 //Make to delete by id
-app.delete('/:id', function(req, res) {
+app.delete('/:id/edit', function(req, res) {
   Picture.destroy({where: {title: req.body.title} })
     .then(function (users) {
-      res.json(users);
+      res.redirect(`/gallery`);
     });
 
 });
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require('express');
 var app = express();
 var bp =  require('body-parser');
 const pug = require('pug');
+const methodOverride = require('method-override');
 
 const pictures = require('./routes/pictures.js');
 var db = require('./models');
 
 app.use(bp.urlencoded({extended : true}));
+
+app.use(methodOverride(function(req, res){
+  console.log('req.body_method: ', req.body._method);
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 var Picture = db.Picture;
 
