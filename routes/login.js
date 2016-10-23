0642f773a7381b76d175cc4d9a097740a7d2183a
peter987bp <peter987bp@gmail.com>
2016-10-22 14:41:00 -1000

    forgot to save code review

--- routes/login.js
+++ routes/login.js
@@ -6,16 +6,14 @@ const passport = require('passport');
 // console.log(req.flash('error'));
   //this message is firing on every page load
   //no username/password at all => { error: [ 'Missing credentials' ] }
+  //BAD username but no password enter => @|##### redirect + missing Credentials
+  //if you a enter a real user but no password =>
 
-  //BAD username but no password enter => @|##### { error: [ 'Missing credentials', 'Missing credentials' ] }
+
 
   //but if you enter a BAD username and BAD password => null
 
-  //if you a enter a real user but no password =>
-   // @|##### { error:
-   // [ 'Missing credentials',
-   //   'Missing credentials',
-   //   'Missing credentials' ] }
+
 
   //if you enter a real user and BAD password
   //Redirct fire and console log =>
@@ -28,10 +26,6 @@ app.get('/', (req,res)=>{
   res.render('login/login');
 });
 
-app.post('/', passport.authenticate('local', {
-  successRedirect: '/gallery',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
+
 
 module.exports = app
